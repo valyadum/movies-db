edebe81f43bf2bce4e52ae248724d0a171280832
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2024-04-06 13:04:47 +0300

    add text

--- src/component/Movies/Movies.tsx
+++ src/component/Movies/Movies.tsx
@@ -431,3 +431,12 @@ export default connector(Movies);
 //   ],
 
 // }
+
+
+// echo "# movies-db" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin git@github.com:valyadum/movies-db.git
+// git push -u origin main
